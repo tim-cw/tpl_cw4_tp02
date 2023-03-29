b62465b8c6213c046300c2ebbac9c59d06e52b2c
Matthieu Parent <matthieuparent@outlook.com>
2023-03-28 21:27:55 -0400

    Updated exercice and stack

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,5 +1,4 @@
 import Icons from './utils/Icons';
-import Carousel from './components/Carousel';
 
 class Main {
   constructor() {
@@ -7,25 +6,8 @@ class Main {
   }
 
   init() {
-    // On ajoute une classe sur le html
-    // pour qu'on puisse savoir si le JavaScript est
-    // supporté dans notre css
-    document.documentElement.classList.add('has-js');
-
-    const components = document.querySelectorAll('[data-component]');
-
-    for (let i = 0; i < components.length; i++) {
-      const element = components[i];
-      const componentName = element.dataset.component;
-
-      if (componentName == 'Carousel') {
-        new Carousel(element);
-      } else {
-        console.log(`La composante ${componentName} n'existe pas`);
-      }
-    }
+    // À toi de jouer ! ;)
 
     Icons.load();
   }
 }
-new Main();
